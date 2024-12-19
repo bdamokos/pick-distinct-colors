511a0669bbf1db92e007c36cfb5e077d51c50cef
bdamokos <163609735+bdamokos@users.noreply.github.com>
2024-12-19 16:52:41 +0100

    use more exported algorithms

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 // Import all the functions we need
 import { maxSumDistancesGlobal, maxSumDistancesSequential } from './algorithms/maxSumDistances.js';
+import { greedySelection } from './algorithms/greedy.js';
+import { simulatedAnnealing } from './algorithms/simulatedAnnealing.js';
+import { kmeansppSelection } from './algorithms/kmeans.js';
+import { geneticAlgorithm } from './algorithms/genetic.js';
+import { particleSwarmOptimization } from './algorithms/particleSwarm.js';
+import { antColonyOptimization } from './algorithms/antColony.js';
+import { tabuSearch } from './algorithms/tabu.js';
 import { rgb2lab, deltaE, sortColors, calculateMetrics, analyzeColorDistribution, rgbToHex } from './utils/colorUtils.js';
 
 // Expose the functions to the window object
 window.algorithms = {
     maxSumDistancesGlobal,
-    maxSumDistancesSequential
+    maxSumDistancesSequential,
+    greedySelection,
+    simulatedAnnealing,
+    kmeansppSelection,
+    geneticAlgorithm,
+    particleSwarmOptimization,
+    antColonyOptimization,
+    tabuSearch
 };
 
 // Expose utility functions
